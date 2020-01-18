5ad48e3b4fce735e01c76181b1afdcf82630ffda
awalhadi006 <awalhadi006@gmail.com>
2020-01-18 17:48:50 +0700

    UAS

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-class MainApp():
+from view.view_book import *
+from view.input_book import *
+from core.baseapp import BaseApp
+
+
+class MainApp(BaseApp):
     def __init__(self):
         self.books = []
+
+    def list_book(self):
+        ViewBook.list(self)
+
+    def add_book(self):
+        InputBook.input(self)
+        InputBook.search(self)
+        SearchHelper.search_title(self)
 
 
 if __name__ == "__main__":
